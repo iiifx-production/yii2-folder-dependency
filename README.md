bc6db00e6ecd1bc6ed6e1c7c29855d0dea55fb4f
Vitaliy IIIFX Khomenko <iiifx@yandex.ru>
2015-06-25 11:32:26 +0300

    Ап

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # yii2-folder-dependency
-Yii2 Folder Dependency
+
+@
---
+++ source/FolderDependency.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace iiifx\cache\dependency;
+
+use InvalidArgumentException;
+
+/**
+ * FileDependency represents a dependency based on a file's last modification time.
+ *
+ * If th last modification time of the file specified via [[fileName]] is changed,
+ * the dependency is considered as changed.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class FolderDependency extends \yii\caching\FileDependency
+{
+    /**
+     * @var string the file path or path alias whose last modification time is used to
+     * check if the dependency has been changed.
+     */
+    public $fileName;
+
+
+    /**
+     * Generates the data needed to determine if dependency has been changed.
+     * This method returns the file's last modification time.
+     * @param Cache $cache the cache component that is currently evaluating this dependency
+     * @return mixed the data needed to determine if dependency has been changed.
+     * @throws InvalidConfigException if [[fileName]] is not set
+     */
+    protected function generateDependencyData($cache)
+    {
+        if ($this->fileName === null) {
+            throw new InvalidConfigException('FileDependency::fileName must be set');
+        }
+
+        return @filemtime(Yii::getAlias($this->fileName));
+    }
+}
